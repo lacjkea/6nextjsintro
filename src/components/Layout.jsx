7969ca497bd5503e1ccd9d833a129240816ea5cf
lacj@kea.dk <lacj@kea.dk>
2023-04-17 13:39:47 +0200

    Layout in _app.js

---
+++ src/components/Layout.jsx
@@ -0,0 +1,42 @@
+import Anchor from "./Anchor";
+export default function Layout({ children }) {
+  return (
+    <>
+      <nav>
+        {" "}
+        <ul>
+          <li>
+            <Anchor href="/">Home</Anchor>
+          </li>
+          <li>
+            <Anchor href="/dogs/henry">Henry</Anchor>
+          </li>
+        </ul>
+      </nav>
+      {/*content */}
+      <main>{children}</main>
+      <footer>FOOTER</footer>
+    </>
+  );
+}
+
+// components/Layout.js
+/* 
+export default function Layout ({children}) {
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li><Anchor href="/">Home</Anchor></li>
+              <li><Anchor href="/dogs/henry">Henry</Anchor></li>
+            </ul>
+          </nav>
+        </header>
+  
+        <main>{children}</main>
+  
+        <footer>Footer content</footer>
+      </>
+    )
+  } */
