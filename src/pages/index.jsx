2c0abd8ddf6993235305ba1df0594ff710191467
lacj@kea.dk <lacj@kea.dk>
2023-04-18 12:35:42 +0200

    bugfixing deploy on Vercel? trying at least...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Anchor from "@/components/Anchor";
 import Link from "next/link";
 import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
